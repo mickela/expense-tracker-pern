3ba5c211206eba6bb62e5a3ac8018007e79f5c02
Michael <osoba.michael@outlook.com>
2020-03-24 19:24:28 +0100

    New controllers
    Implemented routes to fetch all transactions and add transaction

---
+++ controllers/transactions.controller.js
@@ -0,0 +1,78 @@
+const Transaction = require('../models/transactions.model');
+const Sequelize = require('sequelize');
+
+function parse(trans){
+    let sTransactions = JSON.stringify(trans);
+    let pTransactions = JSON.parse(sTransactions);
+    return pTransactions;
+}
+
+
+// @desc Get all transactions
+// @route GET /api/v1/transactions
+// @access public
+
+exports.getTransactions = (req, res, next) => {
+    Transaction.findAll()
+    .then(transactions =>{
+        console.log(transactions);
+        transactions = parse(transactions);
+        console.log(transactions);
+        
+        res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        })
+    })
+    .catch(err =>{
+        console.log(err);
+
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    })
+}
+
+// @desc Add transaction
+// @route POST /api/v1/transactions
+// @access public
+
+exports.addTransaction = (req, res, next) => {
+    // const { text, amount } = req.body;
+
+    console.log(req.body);
+    if(req.body.text !== undefined && req.body.amount !== undefined){
+        Transaction.create(req.body)
+        .then(status =>{
+            console.log(status);
+            
+            res.status(201).json({
+                success: true,
+                data: parse(status)
+            })
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({
+                success: false,
+                error: 'Server Error'
+            });
+        })
+    }else{
+        res.json({
+            success: false,
+            data: "Please send both text and amount!"
+        })
+    }
+}
+
+
+// @desc DELETE all transactions
+// @route DELETE /api/v1/transactions/:id
+// @access public
+
+exports.deleteTransaction = (req, res, next) => {
+    res.json('DELETE transaction');
+}
